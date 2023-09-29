2bd3bdd153a1adb7a201497ca476711e784dc1f0
hezhuLightspeed <he.zhu@lightspeedhq.com>
2023-09-28 22:35:09 -0400

    fix

--- ice-homepage/src/app/contact/page.tsx
+++ ice-homepage/src/app/contact/page.tsx
@@ -1,15 +1,67 @@
-<<<<<<< Updated upstream
-=======
 "use client";
 
 import { useRouter } from "next/navigation";
->>>>>>> Stashed changes
 import Navigation from "../components/navigation";
-
+import Image from "next/image";
 export default function Contact() {
+  const router = useRouter();
+  const handleInstagram = (): void => {
+    console.log("instagram");
+    router.push("https://www.instagram.com/zoeyfrank/");
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center bg-white">
+    <main className="flex min-h-screen flex-col items-center ">
       <Navigation></Navigation>
+      <div className="mt-[50px] flex  w-[1000px] justify-between text-black">
+        <div className="w-1/2">
+          <Image
+            src="/1.jpg"
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className="ml-[50px] w-1/2">
+          <div>
+            <p>
+              Please email me at "zoey@zoeyfrank.com" for a list of available
+              work or other questions, and join my mailing list below to hear
+              about upcoming exhibitions.
+            </p>
+          </div>
+          <div className="flex items-center justify-start">
+            <div
+              className="mt-[30px] flex cursor-pointer flex-row items-center"
+              onClick={handleInstagram}
+            >
+              <Image
+                src="/instagram.png"
+                alt="Picture of the author"
+                width={30}
+                height={30}
+              />
+              <p className="ml-[10px] text-gray-500">Ruobing Liao</p>
+            </div>
+          </div>
+          <div className="mt-[30px] flex flex-col">
+            <h1 className="mb-[20px] text-lg">SUGARLIFT</h1>
+            <div className="space-y-1 text-gray-500">
+              <p>North York</p>
+              <p>Toronto, Ontario</p>
+              <p>hello@sugarlift.com </p>
+            </div>
+          </div>
+          <div className="mt-[30px] flex flex-col">
+            <h1 className="mb-[20px] text-lg">GALERIE MOKUM</h1>
+            <div className="space-y-1 text-gray-500">
+              <p>Oudezijds Voorburgwal 334</p>
+              <p>1012 GM Amsterdam, The Netherlands</p>
+              <p>contact@galeriemokum.com </p>
+              <p>+31 (0)20 - 62 43 958</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- ice-homepage/src/app/paintings/page.tsx
+++ ice-homepage/src/app/paintings/page.tsx
@@ -1,103 +1,6 @@
 "use client";
 
 import Navigation from "../components/navigation";
-<<<<<<< Updated upstream
-import Image from "next/image";
-import {
-  useParams,
-  usePathname,
-  useRouter,
-  useSearchParams,
-} from "next/navigation";
-import { useLastViewedPhoto } from "../utils/useLastViewedPhoto";
-import { useEffect, useRef } from "react";
-import Modal from "../components/Modal";
-import Link from "next/link";
-import { RotatingLines } from "react-loader-spinner";
-
-export const allImages = [
-  {
-    id: 0,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/1.jpg",
-  },
-  {
-    id: 1,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/2.png",
-  },
-  {
-    id: 2,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/3.png",
-  },
-  {
-    id: 3,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/4.jpg",
-  },
-  {
-    id: 4,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/5.jpg",
-  },
-  {
-    id: 5,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/6.jpg",
-  },
-  {
-    id: 6,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/7.jpg",
-  },
-  {
-    id: 7,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/8.jpg",
-  },
-  {
-    id: 8,
-    height: "100",
-    width: "100",
-    public_id: "1",
-    format: "jpg",
-    blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-    imageSrc: "/9.png",
-  },
-=======
 import { useState } from "react";
 import Image from "react-image-enlarger";
 
@@ -113,61 +16,18 @@ const images = [
   "/9.png",
   "/10.jpg",
   "/11.jpg",
->>>>>>> Stashed changes
 ];
 function Paintings() {
-  const [lastViewedPhoto, setLastViewedPhoto] = useLastViewedPhoto();
-
-  const lastViewedPhotoRef = useRef<HTMLAnchorElement>(null);
-
-  const { photoId } = useParams();
-  console.log("Paintings::photoId: ", photoId);
-
-  useEffect(() => {
-    // This effect keeps track of the last viewed photo in the modal to keep the index page in sync when the user navigates back
-    if (lastViewedPhoto && !photoId) {
-      lastViewedPhotoRef.current?.scrollIntoView({ block: "center" });
-      setLastViewedPhoto(null);
-    }
-  }, [photoId, lastViewedPhoto, setLastViewedPhoto]);
+  
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center bg-white">
+      <main className="flex min-h-screen flex-col items-center ">
         <Navigation></Navigation>
-        <div className="mx-auto mt-[50px] max-w-[1000px] p-4">
-          {/* {photoId && (
-            <Modal
-              images={images}
-              onClose={() => {
-                setLastViewedPhoto(photoId as any);
-              }}
-            />
-          )} */}
+        <div className="mx-auto mt-[50px] max-w-[1500px] p-4">
           <div className="c columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-            {allImages.map(({ id, blurDataUrl, imageSrc }) => (
-              <Link
-                className="after:content after:shadow-highlight group relative mb-5 block w-full cursor-zoom-in after:pointer-events-none after:absolute after:inset-0 after:rounded-lg"
-                key={id}
-                href={`/?photoId=${id}`}
-                as={`/p/${id}`}
-                ref={id === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-                shallow
-              >
-                <Image
-                  alt="Ice painting"
-                  className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
-                  style={{ transform: "translate3d(0,0,0" }}
-                  blurDataURL={blurDataUrl}
-                  src={imageSrc}
-                  width={720}
-                  height={480}
-                  sizes="(max-width: 640px) 100vw,
-                  (max-width: 1280px) 50vw,
-                  (max-width: 1536px) 33vw,
-                  25vw"
-                />
-              </Link>
+            {images.map((image) => (
+              <SingleSource key={image} src={image} />
             ))}
           </div>
         </div>
@@ -177,124 +37,20 @@ function Paintings() {
 }
 
 export default Paintings;
+function SingleSource({ src }: { src: string }) {
+  
+  const [zoomed, setZoomed] = useState(false);
 
-export async function getData() {
-  return {
-    props: {
-      images: [
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-        {
-          id: 0,
-          height: "100",
-          width: "100",
-          public_id: "1",
-          format: "jpg",
-          blurDataUrl: "https://w.wallhaven.cc/full/x6/wallhaven-x6pl9v.jpg",
-        },
-      ],
-    },
-  };
+  return (
+    <div style={{ margin: "0.25rem" }}>
+      <Image
+        style={{ width: "500px", height: "auto" }}
+        zoomed={zoomed}
+        src={src}
+        onClick={() => setZoomed(true)}
+        onRequestClose={() => setZoomed(false)}
+        // renderLoading={<ReactLoading type={"cylon"} color="black" />}
+      />
+    </div>
+  );
 }
