c12e219eab167efd1f6e587c7ea27f733b881cb2
赫さん <hezhukevin@gmail.com>
2025-01-10 15:53:43 -0500

    Add exhibition

--- ice-homepage/src/app/about/page.tsx
+++ ice-homepage/src/app/about/page.tsx
@@ -6,28 +6,41 @@ export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center ">
       <Navigation></Navigation>
-      <div className="sm:mt-[50px] flex max-w-[1000px] flex-col items-center justify-between sm:flex-row space-y-5">
-        <div className="flex flex-col mx-5 sm:text-[20px] text-black justify-center items-center sm:mr-[50px] sm:h-[300px] sm:w-3/5">
-          <p className="hidden sm:block sm:text-[25px]">Ruobing Liao</p>
-          <p className="flex justify-center items-center break-words">
-            Ruobing Liao is a Toronto-based contemporary artist with a
-            distinguished Honours Bachelor of Illustration degree from Sheridan
-            College. She specializes in creating striking oil paintings on
-            canvas and delicate watercolors on paper. Through her art, Ruobing
-            actively explores the intricate connections between plants and
-            humans, as well as the boundaries between dreams and reality. She
-            believes that depicting dreams serves as a vital nourishment for her
-            spiritual realm that supplies her inspirations.{" "}
+      <div>
+        <div className="sm:mt-[50px] flex max-w-[1000px] flex-col items-center justify-between sm:flex-row space-y-5">
+          <div className="flex flex-col  sm:text-[20px] text-black justify-center items-center sm:mr-[50px] sm:h-[300px] sm:w-3/5">
+            <p className="hidden sm:block sm:text-[25px]">Ruobing Liao</p>
+            <p className="flex justify-center items-center break-words">
+              Ruobing Liao is a Toronto-based contemporary artist with a
+              distinguished Honours Bachelor of Illustration degree from Sheridan
+              College. She specializes in creating striking oil paintings on
+              canvas and delicate watercolors on paper. Through her art, Ruobing
+              actively explores the intricate connections between plants and
+              humans, as well as the boundaries between dreams and reality. She
+              believes that depicting dreams serves as a vital nourishment for her
+              spiritual realm that supplies her inspirations.{" "}
+            </p>
+          </div>
+          <div className="mx-5 sm:relative sm:h-[300px] sm:w-3/5">
+            <Image
+              src={iceAvatar}
+              alt="ruobingliao"
+              className="object-contain"
+            />
+          </div>
+        </div>
+
+        <div className="mt-[100px] max-w-[1000px]">
+          <h2 className="text-2xl font-bold mb-4">Exhibitions</h2>
+          <p className="mb-4">
+            2024 Artist Project, Better Living Centre, Toronto, ON
+          </p>
+          <p>
+            2024 RAW artist Glitch, Parkdale
           </p>
         </div>
-        <div className="mx-5 sm:relative sm:h-[300px] sm:w-3/5">
-          <Image
-            src={iceAvatar}
-            alt="ruobingliao"
-            className="object-contain"
-          />
-        </div>
       </div>
+
     </main>
   );
 }
--- ice-homepage/src/app/page.tsx
+++ ice-homepage/src/app/page.tsx
@@ -2,8 +2,9 @@ import Navigation from "./components/navigation";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
-      <Navigation></Navigation>
+    <main className="flex min-h-screen flex-col items-center p-6">
+      <Navigation />
+      {/* Add your main content here */}
     </main>
   );
 }
